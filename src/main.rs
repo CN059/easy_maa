c9ac1f9c025804e80414cc7f5144c0f510a6ea26
CN059 <cn059@outlook.com>
2025-11-17 13:23:54 +0800

    style: 修改加载环境变量的方式
    暂时将环境变量写死了做了一下测试，后面计划使用toml配置配置文件

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@ use log;
 use server3::sc_send;
 use std::env;
 use std::error::Error;
+use std::fmt::Debug;
+use std::path::PathBuf;
 use std::process::{Command, exit};
 use std::time::Duration;
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // 只有注册 subscriber 后， 才能在控制台上看到日志输出
-    tracing_subscriber::registry().with(fmt::layer()).init();
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO) // 仅INFO、WARN、ERROR Level的日志会被打印
+        .init();
     // 从 .env 文件加载环境变量。
     // 如果 .env 文件找不到、不可读或无效，则加载失败。
-    dotenvy::dotenv()?;
+    load_env();
     let maa_bin = env::var("MAA_BIN").expect("请在.env文件里设置MAA的二进制路径");
     let container_name = env::var("CONTAINER_NAME").expect("请在.env文件里设置容器名");
     let adb_target = env::var("ADB_TARGET").expect("请在.env文件里设置adb路径");
@@ -26,12 +30,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let maa_state_dir = format!("{}/.local/state", user_home);
     let maa_data_dir = format!("{}/.local/share", user_home);
     let maa_config_dir = format!("{}/.config", user_home);
-    match sc_send(
-        "archMAA".to_string(),
-        "MAA服务准备启动".to_string(),
-    )
-    .await
-    {
+    match sc_send("archMAA".to_string(), "MAA服务准备启动".to_string()).await {
         Ok(_ret) => tracing::info!("Server3酱消息推送成功"),
         Err(_e) => tracing::error!("Server3酱消息推送失败"),
     }
@@ -54,7 +53,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
             log::info!("已找到运行Arknights的容器");
         }
         None => {
-            log::error!("请检查容器是否存在以及.env配置是否正确[提示:你是否使用sudo权限运行该工具?]");
+            log::error!(
+                "请检查容器是否存在以及.env配置是否正确[提示:你是否使用sudo权限运行该工具?]"
+            );
             exit(1);
         }
     }
@@ -79,7 +80,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .output()
         .expect("adb command failed to start");
 
-    log::info!("等待1秒钟");
     tokio::time::sleep(Duration::from_secs(1)).await;
     Command::new("adb")
         .arg("start-server")
@@ -87,6 +87,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .expect("adb command failed to start");
     log::info!("adb已重启");
 
+    log::info!("等待5秒钟模拟器开机");
+    tokio::time::sleep(Duration::from_secs(5)).await;
     // 连接模拟器设备
     let adb = String::from_utf8(
         Command::new("adb")
@@ -98,8 +100,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     )
     .expect("adb command failed to start");
     log::info!("{:?}", adb);
-
-   
 
     // 运行MAA
 
@@ -133,17 +133,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
     );
     log::info!("{:?}", podman);
 
-    match sc_send(
-        "archMAA".to_string(),
-        "MAA运行完毕".to_string(),
-    )
-        .await
-    {
+    match sc_send("archMAA".to_string(), "MAA运行完毕".to_string()).await {
         Ok(_ret) => tracing::info!("Server3酱消息推送成功"),
         Err(_e) => tracing::error!("Server3酱消息推送失败"),
     }
-    
+
     log::info!("已关闭podman容器");
 
     Ok(())
+}
+
+fn load_env() {
+    // 1. 尝试从固定用户配置目录加载：~/.config/easy_maa/.env
+        let config_path = PathBuf::from("/home/cn059/.config/easy_maa/.env");
+
+        log::info!("Trying to load env from: {:?}", config_path);
+
+        if config_path.exists() {
+            dotenvy::from_path(config_path).expect("Failed to load .env from ~/.config/easy_maa/.env");
+            return;
+        }
+
+
+    // 2. 否则回退到当前目录（仅开发模式）
+    #[cfg(debug_assertions)]
+    {
+        dotenvy::dotenv().ok();
+    }
 }
